2e287426f66a92b7697bd73ad6dba4e96362c583
Ali hasan <ali.hasan01@hotmail.com>
2017-07-14 03:07:57 +0500

    Create Node.java

---
+++ chapter2/problem5/source/Node.java
@@ -0,0 +1,11 @@
+package chapter2.problem5.source;
+import chapter2.problem5.source.*;
+
+/**
+ * A simple node class to carrying the data members of nodes of linked list.
+ * Node must have any data and next which linked to the next node.
+*/
+public class Node {
+    public Node next;
+    public int data;
+}
